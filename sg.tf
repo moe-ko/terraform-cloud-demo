54a496a7ec8048a536fadb9e9a123dc1111b739e
Moe <107119323+moe-ko@users.noreply.github.com>
2023-09-29 10:26:52 +0100

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_security_group" "manual-sg" {
+  description = "manual"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "allow http"
+    from_port        = 80
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 80
+    }, {
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "allow https"
+    from_port        = 443
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 443
+    }, {
+    cidr_blocks      = ["172.31.0.0/16"]
+    description      = "allow from private"
+    from_port        = 22
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 22
+  }]
+  name                   = "manual-sg"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = {}
+  tags_all               = {}
+  vpc_id                 = "vpc-06505cccb650351f8"
+}
